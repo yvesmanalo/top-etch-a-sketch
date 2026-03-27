c93d1671a28c12b00628cbb590341929eda4b788
yvesmanalo <23yvesmanalo@gmail.com>
2026-03-27 10:59:18 +0800

    Added initial  dynamic capability for grid creation based on user input

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+const gridSizeInputField = document.querySelector("#input");
 const gridContainer = document.querySelector("#grid-container");
-const gridDimensions = 16 * 16;
+const setGridSizeBtn = document.querySelector("#set-grid-size-btn");
+let gridDimensions = 0;
 
-let i = 1;
-while (i <= gridDimensions) {
-  const individualBox = document.createElement("div");
-  individualBox.setAttribute("class", "box");
-  const innerBox = document.createElement("div");
-  innerBox.setAttribute("class", "inner-box");
-  individualBox.appendChild(innerBox);
+gridSizeInputField.addEventListener("input", (e) => {
+  gridDimensions = e.target.value ** 2;
+});
 
-  gridContainer.appendChild(individualBox);
-  i += 1;
-  if (i === gridDimensions + 1) {
-    break;
+setGridSizeBtn.addEventListener("click", () => {
+  gridContainer.innerHTML = "";
+  let i = 1;
+  while (i <= gridDimensions) {
+    const individualBox = document.createElement("div");
+    individualBox.setAttribute("class", "box");
+    individualBox.style.width = `calc(500px / ${Math.sqrt(gridDimensions)})`;
+    individualBox.style.height = `calc(500px / ${Math.sqrt(gridDimensions)})`;
+    const innerBox = document.createElement("div");
+    innerBox.setAttribute("class", "inner-box");
+    individualBox.appendChild(innerBox);
+    gridContainer.appendChild(individualBox);
+    i += 1;
+    if (i === gridDimensions + 1) {
+      break;
+    }
   }
-}
+});
