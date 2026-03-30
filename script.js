8b730bd80b37db437fe2c5e91133d1d7e88e1af8
yvesmanalo <23yvesmanalo@gmail.com>
2026-03-30 18:20:55 +0800

    Added changing opacity effect on grid mouseover and input validation

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const gridSizeInputField = document.querySelector("#input");
 const gridContainer = document.querySelector("#grid-container");
 const setGridSizeBtn = document.querySelector("#set-grid-size-btn");
+const resetGridBtn = document.querySelector("#reset-grid-btn");
 let gridDimensions = 0;
 
-gridSizeInputField.addEventListener("input", (e) => {
-  gridDimensions = e.target.value ** 2;
+gridSizeInputField.addEventListener("input", () => {
+  gridDimensions = gridSizeInputField.value;
 });
 
 setGridSizeBtn.addEventListener("click", () => {
-  gridContainer.innerHTML = "";
+  resetGrid();
+  setGrid();
+  onGridMouseOVer();
+});
+
+resetGridBtn.addEventListener("click", () => resetGrid());
+
+const setGrid = () => {
+  if (gridDimensions > 100 || gridDimensions <= 0) {
+    alert("Please provide a number between 1 to 100 only");
+    resetGrid();
+    return;
+  }
+  const gridDimensionsSquared = gridDimensions ** 2;
+
   let i = 1;
-  while (i <= gridDimensions) {
+  while (i <= gridDimensionsSquared) {
     const individualBox = document.createElement("div");
     individualBox.setAttribute("class", "box");
-    individualBox.style.width = `calc(500px / ${Math.sqrt(gridDimensions)})`;
-    individualBox.style.height = `calc(500px / ${Math.sqrt(gridDimensions)})`;
+    individualBox.style.width = `calc(600px / ${Math.sqrt(gridDimensionsSquared)})`;
+    individualBox.style.height = `calc(600px / ${Math.sqrt(gridDimensionsSquared)})`;
     const innerBox = document.createElement("div");
     innerBox.setAttribute("class", "inner-box");
     individualBox.appendChild(innerBox);
@@ -24,4 +39,22 @@ setGridSizeBtn.addEventListener("click", () => {
       break;
     }
   }
-});
+};
+
+const onGridMouseOVer = () => {
+  const gridCollection = [...gridContainer.children];
+
+  let op = 0;
+  gridCollection.map((childDiv) =>
+    childDiv.lastElementChild.addEventListener("mouseover", () => {
+      childDiv.lastElementChild.style.backgroundColor = "black";
+      childDiv.lastElementChild.style.opacity = op + 0.01;
+      op += 0.01;
+    }),
+  );
+};
+
+const resetGrid = () => {
+  gridContainer.innerHTML = "";
+  gridSizeInputField.value = "";
+};
